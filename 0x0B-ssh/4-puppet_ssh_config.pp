3ff1212006c14864e8a2bb595c6c71ae72a66f7d
Kenneth-ca <kenneth.ca95@gmail.com>
2019-08-07 03:36:41 +0000

    Corrected path

--- 0x0B-ssh/4-puppet_ssh_config.pp
+++ 0x0B-ssh/4-puppet_ssh_config.pp
@@ -1,18 +1,18 @@
 #Client configuration file with Puppet
-file { '/etc/ssh/sshd_config':
+file { '/etc/ssh/ssh_config':
   ensure => present,
 }
 
 file_line { 'no_pass':
   ensure => present,
-  path => '/etc/ssh/sshd_config',
+  path => '/etc/ssh/ssh_config',
   line => 'PasswordAuthentication no',
   match => '^PasswordAuthentication.*',
 }
 
 file_line { 'identify':
   ensure => present,
-  path => '/etc/ssh/sshd_config',
+  path => '/etc/ssh/ssh_config',
   line => 'IdentifyFile ~/.ssh/holberton',
   match => '^IdentifyFile.*',
 }
