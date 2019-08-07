c53fc71f6043a8122fe7f293e3735b839e2f10d4
Kenneth-ca <kenneth.ca95@gmail.com>
2019-08-07 04:51:49 +0000

    And now?

--- 0x0B-ssh/4-puppet_ssh_config.pp
+++ 0x0B-ssh/4-puppet_ssh_config.pp
@@ -1,15 +1,13 @@
 #Client configuration file with Puppet
 file { '/etc/ssh/ssh_config':
   ensure => present,
-}
-
-file_line { 'no_pass':
+}->
+file_line { 'no password':
   ensure => present,
   path => '/etc/ssh/ssh_config',
   line => 'PasswordAuthentication no',
   match => '^PasswordAuthentication.*',
-}
-
+}->
 file_line { 'identify':
   ensure => present,
   path => '/etc/ssh/ssh_config',
