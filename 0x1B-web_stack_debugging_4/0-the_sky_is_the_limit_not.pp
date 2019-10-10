9ef707421b093e0c985c0e5f3e03440057a6ea9c
Kenneth-cad <kenneth.ca95@gmail.com>
2019-10-10 22:04:13 +0000

    [fix] check scape characters

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,6 +1,6 @@
 # Script that fix the limits for the request of nginx
 exec { 'Debugging4':
   path     => [ '/usr/local/bin:/bin:/usr/bin:/sbin:/usr/sbin:'],
-  command  => "echo 'ULIMIT=\"-n 4096\"' > /etc/default/nginx",
+  command  => "echo 'ULIMIT="-n 4096"' > /etc/default/nginx",
   provider => 'shell',
 }
