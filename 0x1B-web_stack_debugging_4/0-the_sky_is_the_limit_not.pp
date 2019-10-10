fc41f9166a6904272f3666d6592d934552693b46
Kenneth-cad <kenneth.ca95@gmail.com>
2019-10-10 22:24:13 +0000

    [fix] check check

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,11 +1,10 @@
 # Script that fix the limits for the request of nginx
 exec { 'Debugging4':
-  path     => [ '/usr/local/bin:/bin:/usr/bin:/sbin:/usr/sbin:'],
   command  => "echo 'ULIMIT=\"-n 4096\"' > /etc/default/nginx",
-  provider => 'shell',
+  provider => shell,
 }
 
 exec { 'restart':
   command  => 'sudo service nginx restart',
-  provider => 'shell',
+  provider => shell,
 }
