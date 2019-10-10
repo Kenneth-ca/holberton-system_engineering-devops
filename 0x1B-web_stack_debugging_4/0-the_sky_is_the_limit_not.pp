e6dda5b8e7b89e3c1c47e1947719d61741344968
Kenneth-cad <kenneth.ca95@gmail.com>
2019-10-10 21:58:43 +0000

    [add] fix request failed adding more limit

---
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -0,0 +1,6 @@
+# Script that fix the limits for the request of nginx
+exec { 'Debugging4':
+  path     => [ '/usr/local/bin:/bin:/usr/bin:/sbin:/usr/sbin:'],
+  command  => "echo 'ULIMIT=\"-n 4096\"'",
+  provider => 'shell',
+}
